beaceb535790a7c943efa9bc64ee229087c1b242
nooralweli <noor_alwali@hotmail.com>
2021-01-29 19:18:38 +0100

    Delete src directory

--- src/Movable.java
+++
@@ -1,8 +0,0 @@
-/**
- * a interface to move the car
- */
-public interface Movable {
-    void move(); // a method to move in x and y pos
-    void turnLeft(); // a method to turn your direction to left
-    void turnRight(); // a method to turn your direction to right
-}
